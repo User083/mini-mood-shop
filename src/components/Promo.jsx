88e981ccec6534f0d32d72f9614ac287acee9e93
TeliStaffs <97461050+User083@users.noreply.github.com>
2023-07-04 10:18:28 +0100

    Fixes Overlay Pointer Events on Promo Component

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -2,11 +2,11 @@ import { cardigans } from "../constants"
 
 const Promo = () => {
     return (
-        <div className="relative overflow-hidden w-full lg:py-10">
-          <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-            <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-              <div className="sm:max-w-lg">
-                <h1 className="text-4xl font-bold text-primary font-title sm:text-6xl">
+        <div className="relative overflow-hidden w-full lg:py-10 pointer-events-none">
+          <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40 ">
+            <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8  ">
+              <div className="sm:max-w-lg ">
+                <h1 className="text-4xl font-bold text-primary font-title sm:text-6xl ">
                   Something special for the ladies...
                 </h1>
                 <p className="mt-4 text-xl text-black">
@@ -23,8 +23,8 @@ const Promo = () => {
                   >
                     <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
                       <div className="flex items-center space-x-6 lg:space-x-8">
-                        <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                          <div className="h-64 w-44 overflow-hidden rounded sm:opacity-0 lg:opacity-100 shadow-lg hover:scale-110 transition-all">
+                        <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8 ">
+                          <div className="h-64 w-44 overflow-hidden rounded sm:opacity-0 lg:opacity-100 shadow-lg hover:scale-110 transition-all pointer-events-auto">
                              <img
                               src={cardigans[1]}
                               alt="Cardigan"
@@ -32,7 +32,7 @@ const Promo = () => {
                             />  
 
                           </div>
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[2]}
                               alt="Cardigan"
@@ -41,21 +41,21 @@ const Promo = () => {
                           </div>
                         </div>
                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[3]}
                               alt="Cardigan"
                               className="h-full w-full object-cover object-center"
                             />
                           </div>
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[4]}
                               alt="Cardigan"
                               className="h-full w-full object-cover object-center"
                             />
                           </div>
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[5]}
                               alt="Cardigan"
@@ -64,14 +64,14 @@ const Promo = () => {
                           </div>
                         </div>
                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[6]}
                               alt="Cardigan"
                               className="h-full w-full object-cover object-center"
                             />
                           </div>
-                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all">
+                          <div className="h-64 w-44 overflow-hidden rounded shadow-lg hover:scale-110 transition-all pointer-events-auto">
                             <img
                               src={cardigans[7]}
                               alt="Cardigan"
@@ -85,7 +85,7 @@ const Promo = () => {
     
                   <a
                     href="/store"
-                    className="inline-block rounded-md border border-transparent bg-secondary px-8 py-3 text-center font-medium text-white hover:bg-highlight"
+                    className="inline-block rounded-md border border-transparent bg-secondary px-8 py-3 text-center font-medium text-white hover:bg-highlight pointer-events-auto"
                   >
                     Shop Collection
                   </a>
