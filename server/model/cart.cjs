e6dd817ee14d47f2a356a11fc77c9fc6ef8e9aef
User083 <97461050+User083@users.noreply.github.com>
2023-08-07 16:45:27 +0100

    Removing Server to Separate Repo

--- server/model/cart.cjs
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const cartSchema = new mongoose.Schema({
-  item: String,
-  price: String,
-  quantity: Number,
-});
-
-module.exports = mongoose.model("Cart", cartSchema);
