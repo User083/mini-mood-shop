282280ed0aa2998d76fc1b2fafe5f21e9170b369
User083 <97461050+User083@users.noreply.github.com>
2023-08-07 14:17:52 +0100

    Listen Update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const productsRouter = require("./routes/products");
 const cartRouter = require("./routes/cart");
+const PORT = process.env.PORT || 8000;
 
 const cors = require("cors");
 
@@ -11,6 +12,7 @@ async function connect() {
   try {
     await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
     console.log("Connected to db");
+    app.listen(PORT, () => console.log("Server running on " + PORT));
   } catch (error) {
     console.log(error);
   }
@@ -27,9 +29,3 @@ app.use("/products", productsRouter);
 app.use("/cart", cartRouter);
 
 connect();
-
-const port = 8000;
-
-app.listen(port, () =>
-  console.log("Server running on http://localhost:" + port)
-);
